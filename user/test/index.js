6383c62fff3741264292a3ec51ac8d0ce6eef11f
Jose Fernando Valdez Perez <jvaldez@bdgsa.net>
2021-06-18 21:30:30 -0600

    user

--- user/test/index.js
+++ user/test/index.js
@@ -33,10 +33,32 @@ describe('APIs de los usuarios',()=>{
                 .post("/register")
                 .send({
                     name: "Editorial test",
-                    email: "email",
+                    email: "email2",
                     password: "password",
                     address:  "telephone",
                     type:"editorial"
+                })
+                .end((err,response)=>{
+                    response.should.have.status(200); 
+                done();
+                });
+        });
+        it("Deberia borrar un  cliente",(done)=>{
+            chai.request(server)
+                .post("/deleteUser")
+                .send({
+                    email: "email"
+                })
+                .end((err,response)=>{
+                    response.should.have.status(200); 
+                done();
+                });
+        });
+        it("Deberia borrar un editorial",(done)=>{
+            chai.request(server)
+                .post("/deleteUser")
+                .send({
+                    email: "email2"
                 })
                 .end((err,response)=>{
                     response.should.have.status(200); 
